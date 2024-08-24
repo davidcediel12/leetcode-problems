076836011ff504754f579e93ec99156c89d65120
David Cediel <esdago18@gmail.com>
2024-08-24 20:03:13 +0200

    Add the first attempt of longest substring solution

---
+++ src/main/java/com/example/demo/LongestSubString.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class LongestSubString {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        String subString = "";
+        StringBuilder potentialSubString = new StringBuilder();
+        int maxLength = 0;
+        int length = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(potentialSubString.toString().contains(String.valueOf(c))){
+                if(length > maxLength){
+                    subString = potentialSubString.toString();
+                    potentialSubString = new StringBuilder(String.valueOf(s.charAt(i)));
+                    maxLength = length;
+                }
+
+                length = 1;
+
+            } else {
+                length++;
+                potentialSubString.append(c);
+            }
+        }
+        System.out.println(subString);
+        return maxLength;
+    }
+
+
+
+}
---
+++ src/test/java/com/example/demo/LongestSubStringTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongestSubStringTest {
+
+    LongestSubString longestSubString = new LongestSubString();
+
+    @Test
+    void test1(){
+        assertEquals(3, longestSubString.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    @Test
+    void shouldReturnOneWhenAllLettersAreTheSame(){
+        assertEquals(1, longestSubString.lengthOfLongestSubstring("bbbbb"));
+    }
+}
