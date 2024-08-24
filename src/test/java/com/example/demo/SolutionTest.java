1b6e20492cb0f92d578e777a2da7ca7c9285e4a8
David Cediel <esdago18@gmail.com>
2024-08-24 14:52:43 +0200

    Use assertJ to don't force the order of the array in test

--- src/test/java/com/example/demo/SolutionTest.java
+++ src/test/java/com/example/demo/SolutionTest.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SolutionTest {
-
+    
+    Solution solution = new Solution();
 
     @Test
     void testSameNumbers(){
-        var solution = new Solution();
-
         int[] actual = solution.twoSum(new int[]{0, 4, 3, 0}, 0);
         assertArrayEquals(new int[]{0, 3}, actual);
 
@@ -20,9 +20,6 @@ class SolutionTest {
 
     @Test
     void test2(){
-        var solution = new Solution();
-
-
         int[] actual = solution.twoSum(new int[]{3, 2, 4}, 6);
         assertArrayEquals(new int[]{1, 2}, actual);
 
@@ -30,43 +27,31 @@ class SolutionTest {
 
     @Test
     void test3(){
-        var solution = new Solution();
-
-
         int[] actual = solution.twoSum(new int[]{-3,4,3,90}, 0);
-        assertArrayEquals(new int[]{0, 2}, actual);
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(0, 2);
 
     }
 
 
     @Test
     void test4(){
-        var solution = new Solution();
-
-
         int[] actual = solution.twoSum(new int[]{5,75,25}, 100);
-        assertArrayEquals(new int[]{1, 2}, actual);
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(1, 2);
 
     }
 
 
     @Test
     void test5(){
-        var solution = new Solution();
-
-
         int[] actual = solution.twoSum(new int[]{3, 2, 3}, 6);
-        assertArrayEquals(new int[]{0, 2}, actual);
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(0, 2);
     }
 
 
     @Test
     void test6(){
-        var solution = new Solution();
-
-
         int[] actual = solution.twoSum(new int[]{1,6142,8192,10239}, 18431);
-        assertArrayEquals(new int[]{2, 3}, actual);
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(2, 3);
     }
 
 
