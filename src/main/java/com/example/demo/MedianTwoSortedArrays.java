0c490873ff077ca9595f0eeb26c9aaee2a7d83ff
David Cediel <esdago18@gmail.com>
2024-08-24 22:21:03 +0200

    Add first version of median of two arrays

---
+++ src/main/java/com/example/demo/MedianTwoSortedArrays.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MedianTwoSortedArrays {
+
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> completeArray = new ArrayList<>(
+                Arrays.stream(nums1).boxed().toList());
+
+        completeArray.addAll(Arrays.stream(nums2).boxed().toList());
+        completeArray.sort(Integer::compare);
+
+
+        int midPoint = completeArray.size() / 2;
+        if (completeArray.size() % 2 == 0) {
+            return (completeArray.get(midPoint) + completeArray.get(midPoint - 1)) / 2.0;
+        } else {
+            return completeArray.get(midPoint);
+        }
+    }
+}
---
+++ src/test/java/com/example/demo/MedianTwoSortedArraysTest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MedianTwoSortedArraysTest {
+
+    MedianTwoSortedArrays extractor = new MedianTwoSortedArrays();
+
+
+    @Test
+    void shouldReturnMidNumberIfLengthItsPair(){
+        double median = extractor.findMedianSortedArrays(new int[]{1,3}, new int[]{2});
+
+        assertEquals(2.0, median);
+    }
+
+    @Test
+    void shouldReturnMidNumberIfLengthItsImpair(){
+        double median = extractor.findMedianSortedArrays(new int[]{1,2}, new int[]{3, 4});
+
+        assertEquals(2.5, median);
+    }
+}
