ec35eb496caa9a1fe6681ef10690749676f00186
David Cediel <esdago18@gmail.com>
2024-08-28 11:38:25 +0200

    Reduce redundant variables

--- src/main/java/com/example/demo/mediantwosortedarrays/MedianTwoSortedArrays.java
+++ src/main/java/com/example/demo/mediantwosortedarrays/MedianTwoSortedArrays.java
@@ -1,28 +1,13 @@
 package com.example.demo.mediantwosortedarrays;
 
-import java.util.HashSet;
-
 public class MedianTwoSortedArrays {
 
 
-    private boolean finishedFirstArray;
-    private boolean finishedSecondArray;
-    int currentNumber;
-    int[] nums1;
-    int[] nums2;
-    int totalLength;
-    boolean isLengthEven;
-
-    int i;
-    int j;
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        this.nums1 = nums1;
-        this.nums2 = nums2;
 
-        totalLength = nums1.length + nums2.length;
-        isLengthEven = totalLength % 2 == 0;
+        int totalLength = nums1.length + nums2.length;
+        boolean isLengthEven = totalLength % 2 == 0;
 
         int midPoint;
         int position = 0;
@@ -33,21 +18,31 @@ public class MedianTwoSortedArrays {
             midPoint = totalLength / 2;
         }
 
-        i = 0;
-        j = 0;
+        int i = 0;
+        int j = 0;
+        int currentNumber;
 
         double counter = 0;
         boolean modifiedCounter = false;
 
-        finishedFirstArray = nums1.length == 0;
-        finishedSecondArray = nums2.length == 0;
+        while (i < nums1.length || j < nums2.length) {
 
-        HashSet<Integer> usedIndexesFirstArr = new HashSet<>();
-        HashSet<Integer> usedIndexesSecondArr = new HashSet<>();
+            if (j >= nums2.length) {
+                currentNumber = nums1[i];
+                i++;
+            } else if (i >= nums1.length) {
+                currentNumber = nums2[j];
+                j++;
+            } else {
+                if (nums1[i] <= nums2[j]) {
+                    currentNumber = nums1[i];
+                    i++;
+                } else {
+                    currentNumber = nums2[j];
+                    j++;
+                }
+            }
 
-
-        while (!finishedFirstArray || !finishedSecondArray) {
-            findCurrentNumber(usedIndexesFirstArr, usedIndexesSecondArr);
             if (position >= midPoint) {
                 if (isLengthEven) {
                     if (!modifiedCounter) {
@@ -60,47 +55,8 @@ public class MedianTwoSortedArrays {
                     return currentNumber;
                 }
             }
-
-            boolean moveFirstIndex = usedIndexesFirstArr.contains(i) && i + 1 < nums1.length;
-            boolean moveSecondIndex = usedIndexesSecondArr.contains(j) && j + 1 < nums2.length;
-            if (moveFirstIndex) {
-                i++;
-            } else if (moveSecondIndex) {
-                j++;
-            }
             position++;
         }
         return 0;
-    }
-
-
-    private void findCurrentNumber(HashSet<Integer> usedIndexesFirstArr,
-                                   HashSet<Integer> usedIndexesSecondArr) {
-
-        if (finishedSecondArray) {
-            currentNumber = nums1[i];
-            usedIndexesFirstArr.add(i);
-
-            finishedFirstArray = i + 1 == nums1.length;
-
-        } else if (finishedFirstArray) {
-            currentNumber = nums2[j];
-            usedIndexesSecondArr.add(j);
-
-            finishedSecondArray = j + 1 == nums2.length;
-        } else {
-
-            if (usedIndexesSecondArr.contains(j) ||
-                    (nums1[i] <= nums2[j] && !usedIndexesFirstArr.contains(i))) {
-
-                currentNumber = nums1[i];
-                usedIndexesFirstArr.add(i);
-                finishedFirstArray = i == nums1.length - 1;
-            } else {
-                currentNumber = nums2[j];
-                usedIndexesSecondArr.add(j);
-                finishedSecondArray = j == nums2.length - 1;
-            }
-        }
     }
 }
