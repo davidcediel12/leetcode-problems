8480cbd6e771361c1cb591dbdfca2722353f0277
David Cediel <esdago18@gmail.com>
2024-09-14 18:59:14 +0200

    Reversing the number using remained

--- src/main/java/com/example/demo/reverseinteger/ReverseInteger.java
+++ src/main/java/com/example/demo/reverseinteger/ReverseInteger.java
@@ -21,4 +21,39 @@ public class ReverseInteger {
         }
 
     }
+
+
+    public int reverse2(int x) {
+        boolean isNegative = x < 0;
+        if (isNegative) {
+            x = x * -1;
+        }
+
+        int number = 0;
+        while (x >= 10) {
+            int b = x % 10;
+            if (tooBig(number, b)) {
+                return 0;
+            }
+            number = (number * 10) + b;
+            x = x / 10;
+        }
+
+
+        if (tooBig(number, x)) {
+            return 0;
+        }
+        number = (number * 10) + x;
+
+
+        if (isNegative) {
+            return number * -1;
+        }
+
+        return number;
+    }
+
+    boolean tooBig(int number, int x) {
+        return number > Integer.MAX_VALUE / 10 || number * 10 > Integer.MAX_VALUE - x;
+    }
 }
--- src/test/java/com/example/demo/reverseinteger/ReverseIntegerTest.java
+++ src/test/java/com/example/demo/reverseinteger/ReverseIntegerTest.java
@@ -11,30 +11,37 @@ class ReverseIntegerTest {
     @Test
     void shouldReverseProperly(){
 
-        Integer actual = reverseInteger.reverse(123);
+        Integer actual = reverseInteger.reverse2(123);
         assertEquals(321, actual);
     }
 
     @Test
     void shouldReverse120Properly(){
 
-        Integer actual = reverseInteger.reverse(120);
+        Integer actual = reverseInteger.reverse2(120);
         assertEquals(21, actual);
     }
 
     @Test
     void shouldReverseProperlyWhenNegativeNumber(){
 
-        Integer actual = reverseInteger.reverse(-123);
+        Integer actual = reverseInteger.reverse2(-123);
         assertEquals(-321, actual);
     }
 
 
     @Test
-    void shouldR2(){
+    void shouldReturnZeroWhenNumberIsTooBig(){
 
-        Integer actual = reverseInteger.reverse(1534236469);
+        Integer actual = reverseInteger.reverse2(1534236469);
         assertEquals(0, actual);
+    }
+
+    @Test
+    void shouldReturnOne(){
+
+        Integer actual = reverseInteger.reverse2(10);
+        assertEquals(1, actual);
     }
 
 }
