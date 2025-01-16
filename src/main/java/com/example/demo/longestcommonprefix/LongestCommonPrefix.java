f0b8c36dfac97f9b48dd5ad9595edb41c8a6f70c
David Cediel <esdago18@gmail.com>
2025-01-16 20:01:40 +0100

    Add another option to just go through the strings once

--- src/main/java/com/example/demo/longestcommonprefix/LongestCommonPrefix.java
+++ src/main/java/com/example/demo/longestcommonprefix/LongestCommonPrefix.java
@@ -1,11 +1,13 @@
 package com.example.demo.longestcommonprefix;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class LongestCommonPrefix {
 
-    public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix2(String[] strs) {
 
         String minWord = Arrays.stream(strs)
                 .min(Comparator.comparingInt(String::length))
@@ -32,5 +34,45 @@ public class LongestCommonPrefix {
         }
 
         return commonPrefix;
+    }
+
+
+    public String longestCommonPrefix(String[] strs) {
+
+        List<String> stringList = new ArrayList<>(Arrays.stream(strs).toList());
+
+        String minWord = Arrays.stream(strs)
+                .min(Comparator.comparingInt(String::length))
+                .orElse("");
+
+
+        stringList.remove(minWord);
+
+        String bestPrefix = minWord;
+
+        int i = 0;
+        while (i < stringList.size()) {
+
+            int j = 0;
+            boolean isMatch = true;
+            String prefix = "";
+            while (j < minWord.length() && isMatch) {
+                if (minWord.charAt(j) != stringList.get(i).charAt(j)) {
+                    prefix = minWord.substring(0, j);
+                    isMatch = false;
+                }
+                j++;
+            }
+            if(isMatch){
+                prefix = minWord;
+            }
+
+            if (prefix.length() < bestPrefix.length()) {
+                bestPrefix = prefix;
+            }
+
+            i++;
+        }
+        return bestPrefix;
     }
 }
