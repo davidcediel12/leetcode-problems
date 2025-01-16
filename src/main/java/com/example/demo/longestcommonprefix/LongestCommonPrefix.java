e206a18abda28381570e970750cc0776c21979a5
David Cediel <esdago18@gmail.com>
2025-01-16 19:42:10 +0100

    Use the smallest word to make the comparison

--- src/main/java/com/example/demo/longestcommonprefix/LongestCommonPrefix.java
+++ src/main/java/com/example/demo/longestcommonprefix/LongestCommonPrefix.java
@@ -1,32 +1,36 @@
 package com.example.demo.longestcommonprefix;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
 
-        String firstWord = strs[0];
+        String minWord = Arrays.stream(strs)
+                .min(Comparator.comparingInt(String::length))
+                .orElse("");
 
         String commonPrefix = "";
         boolean isPrefix = true;
         int i = 1;
 
-        while(isPrefix && i <= firstWord.length()){
+        while (isPrefix && i <= minWord.length()) {
 
-            String prefix = firstWord.substring(0, i);
+            String prefix = minWord.substring(0, i);
 
             isPrefix = Arrays.stream(strs)
                     .parallel()
                     .allMatch(str -> str.startsWith(prefix));
 
 
-            if(isPrefix) {
+            if (isPrefix) {
                 commonPrefix = prefix;
             }
             i++;
 
         }
+
         return commonPrefix;
     }
 }
