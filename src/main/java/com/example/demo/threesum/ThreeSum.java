392e7479e71eb274ca444320758d798873e73877
David Cediel <esdago18@gmail.com>
2025-01-25 11:37:19 +0100

    Reduce the last loop to find the triplet

--- src/main/java/com/example/demo/threesum/ThreeSum.java
+++ src/main/java/com/example/demo/threesum/ThreeSum.java
@@ -6,24 +6,26 @@ public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
 
+        List<Integer> numbers = Arrays.stream(nums).boxed().toList();
+
         Set<List<Integer>> triplets = new HashSet<>();
         Set<List<Integer>> orderedTriplets = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
+            int t1 = nums[i];
             for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
+                int t2 = nums[j];
+                int t3 = -t1 - t2;
 
-                    int t1 = nums[i];
-                    int t2 = nums[j];
-                    int t3 = nums[k];
+                List<Integer> possibleTriplet = List.of(t1, t2, t3);
+                List<Integer> orderedPossibleTriplet = possibleTriplet.stream().sorted().toList();
 
-                    List<Integer> possibleTriplet = List.of(t1, t2, t3);
-                    List<Integer> orderedPossibleTriplet = possibleTriplet.stream().sorted().toList();
+                boolean existsT3 = numbers.subList(j + 1, numbers.size()).contains(t3);
+                boolean notRepeatedTriplet = !orderedTriplets.contains(orderedPossibleTriplet);
 
-                    if (t1 + t2 + t3 == 0 && !orderedTriplets.contains(orderedPossibleTriplet)) {
-                        triplets.add(possibleTriplet);
-                        orderedTriplets.add(orderedPossibleTriplet);
-                    }
+                if(j + 1 < nums.length && notRepeatedTriplet && existsT3){
+                    triplets.add(possibleTriplet);
+                    orderedTriplets.add(orderedPossibleTriplet);
                 }
             }
         }
