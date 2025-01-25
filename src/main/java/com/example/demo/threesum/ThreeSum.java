dff7b49f112b9c99b1973c33cacdf55de486569a
David Cediel <esdago18@gmail.com>
2025-01-25 10:17:45 +0100

    Add problem 15 readme and skeleton class

---
+++ src/main/java/com/example/demo/threesum/ThreeSum.java
@@ -0,0 +1,11 @@
+package com.example.demo.threesum;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return Collections.emptyList();
+    }
+}
