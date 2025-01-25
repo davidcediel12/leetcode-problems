7154f1c4fb31491d89b20f637b4f70152375f828
David Cediel <esdago18@gmail.com>
2025-01-25 11:04:45 +0100

    Add brute force solution for problem 15

--- src/main/java/com/example/demo/threesum/ThreeSum.java
+++ src/main/java/com/example/demo/threesum/ThreeSum.java
@@ -1,11 +1,32 @@
 package com.example.demo.threesum;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        return Collections.emptyList();
+
+        Set<List<Integer>> triplets = new HashSet<>();
+        Set<List<Integer>> orderedTriplets = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+
+                    int t1 = nums[i];
+                    int t2 = nums[j];
+                    int t3 = nums[k];
+
+                    List<Integer> possibleTriplet = List.of(t1, t2, t3);
+                    List<Integer> orderedPossibleTriplet = possibleTriplet.stream().sorted().toList();
+
+                    if (t1 + t2 + t3 == 0 && !orderedTriplets.contains(orderedPossibleTriplet)) {
+                        triplets.add(possibleTriplet);
+                        orderedTriplets.add(orderedPossibleTriplet);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(triplets);
     }
 }
