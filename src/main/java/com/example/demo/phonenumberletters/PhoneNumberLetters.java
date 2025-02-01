9a3209efec707eb8c61658a0e8662ad84546ebe5
David Cediel <esdago18@gmail.com>
2025-02-01 10:58:53 +0100

    Add skeleton of problem 17

---
+++ src/main/java/com/example/demo/phonenumberletters/PhoneNumberLetters.java
@@ -0,0 +1,11 @@
+package com.example.demo.phonenumberletters;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PhoneNumberLetters {
+
+    public List<String> letterCombinations(String digits) {
+        return Collections.emptyList();
+    }
+}
