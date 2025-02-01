97ff28ff37af69b0b10751f7c747332b12974d49
David Cediel <esdago18@gmail.com>
2025-02-01 13:06:01 +0100

    Add skeleton of problem 18

---
+++ src/main/java/com/example/demo/foursum/FourSum.java
@@ -0,0 +1,11 @@
+package com.example.demo.foursum;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return Collections.emptyList();
+    }
+}
