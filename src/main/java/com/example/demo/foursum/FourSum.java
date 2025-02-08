d015bfe32d48c3c5cdfb4b67a33bc90b47f328ef
David Cediel <esdago18@gmail.com>
2025-02-08 12:35:32 +0100

    Skip iterations where the starting point was already processed

--- src/main/java/com/example/demo/foursum/FourSum.java
+++ src/main/java/com/example/demo/foursum/FourSum.java
@@ -11,7 +11,15 @@ public class FourSum {
         Set<List<Integer>> quadruplets = new HashSet<>();
 
         for (int left = 0; left < numbers.size(); left++) {
+
+            if (left > 0 && numbers.get(left).equals(numbers.get(left - 1))) {
+                continue;
+            }
             for (int left2 = left + 1; left2 < numbers.size() - 2; left2++) {
+
+                if (left2 > left + 1 && numbers.get(left2).equals(numbers.get(left2 - 1))) {
+                    continue;
+                }
 
                 int leftChild = left2 + 1;
                 int rightChild = numbers.size() - 1;
@@ -27,9 +35,13 @@ public class FourSum {
                         leftChild++;
                         rightChild--;
                     } else if (sum > target) {
-                        rightChild--;
+                        do {
+                            rightChild--;
+                        } while (rightChild > leftChild && numbers.get(rightChild).equals(numbers.get(rightChild + 1)));
                     } else {
-                        leftChild++;
+                        do {
+                            leftChild++;
+                        } while (leftChild < rightChild && numbers.get(left).equals(numbers.get(leftChild - 1)));
                     }
                 }
             }
