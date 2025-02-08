65827a91c46d09be01d9b651ad3935946f348866
David Cediel <esdago18@gmail.com>
2025-02-08 13:10:53 +0100

    Create problem 19 readme and first possible solution

---
+++ src/main/java/com/example/demo/removennode/RemoveNNode.java
@@ -0,0 +1,55 @@
+package com.example.demo.removennode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveNNode {
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        List<ListNode> allNodes = new ArrayList<>();
+        allNodes.add(head);
+
+        while (head.next != null) {
+            head = head.next;
+            allNodes.add(head);
+        }
+
+        int indexToDelete = allNodes.size() - n;
+
+        if (indexToDelete - 1 == 0) {
+            if (allNodes.size() == 1) {
+                return null;
+            } else {
+                return allNodes.get(1);
+            }
+        }
+
+        if (indexToDelete == allNodes.size() - 1) {
+            allNodes.get(allNodes.size() - 2).next = null;
+            return allNodes.getFirst();
+        }
+
+        allNodes.get(indexToDelete - 1).next = allNodes.get(indexToDelete + 1);
+        return allNodes.getFirst();
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
