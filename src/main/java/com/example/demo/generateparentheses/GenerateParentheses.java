9f24894055b761be2119825a7c0a86456b0f2374
David Cediel <esdago18@gmail.com>
2025-02-15 11:59:11 +0100

    Create skeleton and readme for problem 22

---
+++ src/main/java/com/example/demo/generateparentheses/GenerateParentheses.java
@@ -0,0 +1,12 @@
+package com.example.demo.generateparentheses;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GenerateParentheses {
+
+
+    public List<String> generateParenthesis(int n) {
+        return Collections.emptyList();
+    }
+}
