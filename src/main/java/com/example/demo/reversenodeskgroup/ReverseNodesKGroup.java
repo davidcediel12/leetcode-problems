03b36716eeff7d3be52fab66444628804385b592
David Cediel <esdago18@gmail.com>
2025-03-01 12:30:20 +0100

    Start coding the problem 25

--- src/main/java/com/example/demo/reversenodeskgroup/ReverseNodesKGroup.java
+++ src/main/java/com/example/demo/reversenodeskgroup/ReverseNodesKGroup.java
@@ -6,7 +6,66 @@ import java.util.Objects;
 public class ReverseNodesKGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+
+        if(k == 1){
+            return head;
+        }
+        ListNode iterator = head;
+
+        ListNode past = null;
+        ListNode pastPast = null; // To reconnect previous node to the swapped 1 (past past) --> 4 (iterator) --> 3 (past)
+        ListNode nodeToSwap = null;
+
+
+        int i = 1;
+        boolean isMomentToChange;
+        boolean changedHead = false;
+
+        while (iterator != null) {
+
+            isMomentToChange = i == k;
+
+            if (isMomentToChange) {
+                if (pastPast != null) {
+                    pastPast.next = iterator;
+                }
+
+                ListNode next = iterator.next;
+
+                if (k == 2) {
+                    iterator.next = past;
+                    past.next = next;
+                } else {
+                    past.next = nodeToSwap;
+                    iterator.next = nodeToSwap.next;
+                    nodeToSwap.next = next;
+                }
+
+                if (!changedHead) {
+                    head = iterator;
+                    changedHead = true;
+                }
+
+                while(i > 1){
+                    iterator = iterator.next;
+                    i--;
+                }
+                pastPast = iterator;
+
+                i = 0;
+
+            } else if (i == k - 1) {
+                past = iterator;
+            }
+
+            if (i == 1) {
+                nodeToSwap = iterator;
+            }
+            iterator = iterator == null ? null : iterator.next;
+            i++;
+        }
+
+        return head;
     }
 
     public static class ListNode {
---
+++ src/test/java/com/example/demo/reversenodeskgroup/ReverseNodesKGroupTest.java
@@ -0,0 +1,75 @@
+package com.example.demo.reversenodeskgroup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReverseNodesKGroupTest {
+
+    ReverseNodesKGroup reverseNodesKGroup = new ReverseNodesKGroup();
+
+
+    @Test
+    void shouldSwapCorrectly() {
+
+        ReverseNodesKGroup.ListNode initial = new ReverseNodesKGroup.ListNode(1,
+                new ReverseNodesKGroup.ListNode(2, new ReverseNodesKGroup.ListNode(3,
+                        new ReverseNodesKGroup.ListNode(4))));
+
+
+        ReverseNodesKGroup.ListNode expected = new ReverseNodesKGroup.ListNode(2,
+                new ReverseNodesKGroup.ListNode(1, new ReverseNodesKGroup.ListNode(4,
+                        new ReverseNodesKGroup.ListNode(3))));
+
+
+        var actual = reverseNodesKGroup.reverseKGroup(initial, 2);
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void shouldSwapCorrectly2() {
+
+        ReverseNodesKGroup.ListNode initial = new ReverseNodesKGroup.ListNode(1,
+                new ReverseNodesKGroup.ListNode(2, new ReverseNodesKGroup.ListNode(3,
+                        new ReverseNodesKGroup.ListNode(4,
+                                new ReverseNodesKGroup.ListNode(5)))));
+
+
+        ReverseNodesKGroup.ListNode expected = new ReverseNodesKGroup.ListNode(3,
+                new ReverseNodesKGroup.ListNode(2, new ReverseNodesKGroup.ListNode(1,
+                        new ReverseNodesKGroup.ListNode(4,
+                                new ReverseNodesKGroup.ListNode(5)))));
+
+
+        var actual = reverseNodesKGroup.reverseKGroup(initial, 3);
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void shouldSwapCorrectly3() {
+
+        ReverseNodesKGroup.ListNode initial = new ReverseNodesKGroup.ListNode(1,
+                new ReverseNodesKGroup.ListNode(2, new ReverseNodesKGroup.ListNode(3,
+                        new ReverseNodesKGroup.ListNode(4,
+                                new ReverseNodesKGroup.ListNode(5)))));
+
+
+        ReverseNodesKGroup.ListNode expected = new ReverseNodesKGroup.ListNode(3,
+                new ReverseNodesKGroup.ListNode(2, new ReverseNodesKGroup.ListNode(1,
+                        new ReverseNodesKGroup.ListNode(4,
+                                new ReverseNodesKGroup.ListNode(5)))));
+
+
+        var actual = reverseNodesKGroup.reverseKGroup(initial, 1);
+
+        assertEquals(expected, actual);
+
+    }
+
+}
