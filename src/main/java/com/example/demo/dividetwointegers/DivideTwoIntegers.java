281b490a985c8759a1f430bb21085ae89802a0f9
David Cediel <esdago18@gmail.com>
2025-03-08 14:10:48 +0100

    Divide method by test case

--- src/main/java/com/example/demo/dividetwointegers/DivideTwoIntegers.java
+++ src/main/java/com/example/demo/dividetwointegers/DivideTwoIntegers.java
@@ -3,18 +3,59 @@ package com.example.demo.dividetwointegers;
 public class DivideTwoIntegers {
 
     public int divide(int dividend, int divisor) {
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+            return divideBothSameSign(dividend, divisor);
+        }
+
+        if (dividend < 0) {
+            return divideDividenNegative(dividend, divisor);
+        }
+
+        return divideDivisorNegative(dividend, divisor);
+    }
+
+    public int divideBothSameSign(int dividend, int divisor) {
 
         int quotient = 0;
-        boolean changeSign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
 
-        dividend = Math.abs(dividend);
-        divisor = Math.abs(divisor);
+        boolean dividendSmaller = dividend < divisor;
 
-        while (dividend >= divisor){
+        while (quotient < Integer.MAX_VALUE && ((dividendSmaller && dividend <= divisor) ||
+                (!dividendSmaller && dividend >= divisor))) {
             dividend -= divisor;
             quotient++;
         }
-        return changeSign ? quotient * -1 : quotient;
+        return quotient;
 
     }
+
+    int divideDividenNegative(int dividend, int divisor) {
+
+        int divisorNegative = -divisor;
+        int quotient = 0;
+        while (dividend <= divisorNegative) {
+
+            dividend += divisor;
+            quotient--;
+        }
+
+        return quotient;
+    }
+
+
+    int divideDivisorNegative(int dividend, int divisor) {
+
+        int divisorPositive = -divisor;
+        int quotient = 0;
+
+        while (dividend >= divisorPositive) {
+
+            dividend += divisor;
+            quotient--;
+        }
+
+        return quotient;
+    }
+
+
 }
--- src/test/java/com/example/demo/dividetwointegers/DivideTwoIntegersTest.java
+++ src/test/java/com/example/demo/dividetwointegers/DivideTwoIntegersTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.dividetwointegers;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -21,6 +22,23 @@ class DivideTwoIntegersTest {
         int quotient = divideTwoIntegers.divide(dividend, divisor);
 
         assertEquals(expectedQuotient, quotient);
+    }
+
+
+    @Test
+    void shouldDivideCorrectly2() {
+
+        int quotient = divideTwoIntegers.divide(-2147483648, -1);
+
+        assertEquals(2147483647, quotient);
+    }
+
+    @Test
+    void shouldDivideCorrectly3() {
+
+        int quotient = divideTwoIntegers.divide(7, -3);
+
+        assertEquals(-2, quotient);
     }
 
 }
