cd2573d2986e7d8fc00f5356c33fb1f69dee4b23
David Cediel <esdago18@gmail.com>
2025-03-08 12:48:58 +0100

    Create a shorter option using streams (slower option according to leetcode judge)

--- src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
+++ src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
@@ -1,37 +1,39 @@
 package com.example.demo.removeduplicatessortedarray;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RemoveDuplicates {
 
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates2(int[] nums) {
 
         Set<Integer> existing = new HashSet<>();
         int toSwap;
         int virtualEnd = nums.length;
 
-        for(int i = 0; i < virtualEnd; i++){
+        for (int i = 0; i < virtualEnd; i++) {
 
-            if(existing.contains(nums[i])){
+            if (existing.contains(nums[i])) {
                 toSwap = nums[i];
 
                 int j = i + 1;
-                while (j < virtualEnd && nums[j] == toSwap){
+                while (j < virtualEnd && nums[j] == toSwap) {
                     j++;
                 }
 
                 boolean noDifferentNumbers = j == virtualEnd;
 
-                if(noDifferentNumbers){
+                if (noDifferentNumbers) {
                     return existing.size();
                 }
 
                 existing.add(nums[j]);
                 int positionsToSwap = j - i;
 
-                while (j < virtualEnd){
-                    nums[j-positionsToSwap] = nums[j];
+                while (j < virtualEnd) {
+                    nums[j - positionsToSwap] = nums[j];
                     j++;
                 }
 
@@ -43,4 +45,21 @@ public class RemoveDuplicates {
         }
         return existing.size();
     }
+
+
+    public int removeDuplicates(int[] nums) {
+
+
+        List<Integer> distinctNumbers = Arrays.stream(nums)
+                .boxed()
+                .distinct().toList();
+
+
+        for (int i = 0; i < distinctNumbers.size(); i++) {
+            nums[i] = distinctNumbers.get(i);
+        }
+
+        return distinctNumbers.size();
+    }
+
 }
