c7115df0fab47810923cd36f9d486e1698252a5d
David Cediel <esdago18@gmail.com>
2025-03-08 11:55:53 +0100

    Add first implementation of problem 26

--- src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
+++ src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
@@ -1,8 +1,46 @@
 package com.example.demo.removeduplicatessortedarray;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDuplicates {
 
     public int removeDuplicates(int[] nums) {
-        return 0;
+
+        Set<Integer> existing = new HashSet<>();
+        int toSwap;
+        int virtualEnd = nums.length;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(existing.contains(nums[i])){
+                toSwap = nums[i];
+
+                int j = i + 1;
+                while (j < virtualEnd && nums[j] != toSwap){
+                    j++;
+                }
+
+                boolean noDifferentNumbers = j == virtualEnd;
+
+                if(noDifferentNumbers){
+                    return nums[i];
+                }
+
+
+                int positionsToSwap = j - i;
+
+                while (j < virtualEnd){
+                    nums[j-positionsToSwap] = nums[j];
+                    j++;
+                }
+
+                virtualEnd = virtualEnd - positionsToSwap;
+
+            } else {
+                existing.add(nums[i]);
+            }
+        }
+        return existing.size();
     }
 }
