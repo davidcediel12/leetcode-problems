4805d48ebb681508f552e937a4f9f1115dc06c3d
David Cediel <esdago18@gmail.com>
2025-03-08 12:30:05 +0100

    Use virtual end to end the loop, fix existing set

--- src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
+++ src/main/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicates.java
@@ -11,13 +11,13 @@ public class RemoveDuplicates {
         int toSwap;
         int virtualEnd = nums.length;
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < virtualEnd; i++){
 
             if(existing.contains(nums[i])){
                 toSwap = nums[i];
 
                 int j = i + 1;
-                while (j < virtualEnd && nums[j] != toSwap){
+                while (j < virtualEnd && nums[j] == toSwap){
                     j++;
                 }
 
@@ -27,7 +27,7 @@ public class RemoveDuplicates {
                     return nums[i];
                 }
 
-
+                existing.add(nums[j]);
                 int positionsToSwap = j - i;
 
                 while (j < virtualEnd){
---
+++ src/test/java/com/example/demo/removeduplicatessortedarray/RemoveDuplicatesTest.java
@@ -0,0 +1,45 @@
+package com.example.demo.removeduplicatessortedarray;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RemoveDuplicatesTest {
+
+    RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+
+
+    @Test
+    void shouldRemoveDuplicatesCorrectly() {
+
+        int[] expected = new int[]{1, 2};
+
+        int[] input = new int[]{1, 1, 2};
+
+        int actualK = removeDuplicates.removeDuplicates(input);
+
+        assertCorrectness(actualK, expected, input);
+    }
+
+    @Test
+    void shouldRemoveDuplicatesCorrectly2() {
+
+        int[] expected = new int[]{0, 1, 2, 3, 4};
+
+        int[] input = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int actualK = removeDuplicates.removeDuplicates(input);
+
+        assertCorrectness(actualK, expected, input);
+    }
+
+
+    private static void assertCorrectness(int actualK, int[] expected, int[] input) {
+        assertEquals(expected.length, actualK);
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], input[i]);
+        }
+    }
+
+}
