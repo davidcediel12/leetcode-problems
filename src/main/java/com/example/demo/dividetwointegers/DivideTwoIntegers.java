47dd8f3570f65c2f3c1ef37435b97ef3f1d2b1a9
David Cediel <esdago18@gmail.com>
2025-04-12 09:52:10 +0200

    Add flag if the divisor is one

--- src/main/java/com/example/demo/dividetwointegers/DivideTwoIntegers.java
+++ src/main/java/com/example/demo/dividetwointegers/DivideTwoIntegers.java
@@ -12,6 +12,10 @@ public class DivideTwoIntegers {
             return 1;
         }
 
+        if(divisor == 1){
+            return dividend;
+        }
+
         if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
             return divideBothSameSign(dividend, divisor);
         }
