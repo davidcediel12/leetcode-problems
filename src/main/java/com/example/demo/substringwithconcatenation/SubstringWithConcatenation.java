8d872f7d5b5ee66891885bbb782c9294d7f9e635
David Cediel <esdago18@gmail.com>
2025-04-12 12:54:46 +0200

    Add skeleton of problem 30

---
+++ src/main/java/com/example/demo/substringwithconcatenation/SubstringWithConcatenation.java
@@ -0,0 +1,11 @@
+package com.example.demo.substringwithconcatenation;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SubstringWithConcatenation {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        return Collections.emptyList();
+    }
+}
