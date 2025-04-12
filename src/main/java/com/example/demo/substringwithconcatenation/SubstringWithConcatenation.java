f512948bd4c699da68286dbcaf9199a7300abcc2
David Cediel <esdago18@gmail.com>
2025-04-12 13:15:31 +0200

    Add first raw implementation of problem 30

--- src/main/java/com/example/demo/substringwithconcatenation/SubstringWithConcatenation.java
+++ src/main/java/com/example/demo/substringwithconcatenation/SubstringWithConcatenation.java
@@ -1,11 +1,47 @@
 package com.example.demo.substringwithconcatenation;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SubstringWithConcatenation {
 
     public List<Integer> findSubstring(String s, String[] words) {
-        return Collections.emptyList();
+
+        Set<String> wordsSet = new HashSet<>(Arrays.stream(words).toList());
+        int wordLength = words[0].length();
+
+        List<Integer> indexes = new ArrayList<>();
+
+
+
+        for(int i = 0; i < s.length(); i += wordLength){
+            checkConcatenatedString(s, wordsSet, i, wordLength, indexes);
+        }
+
+        return indexes;
+
+    }
+
+    private void checkConcatenatedString(String s, Set<String> wordsSet,
+                                         int actualIndex, int wordLength, List<Integer> indexes) {
+
+       Set<String> copyWordSet = new HashSet<>(wordsSet);
+
+       int movingIndex = actualIndex;
+
+       while(!copyWordSet.isEmpty() && movingIndex <= s.length()){
+
+           String word = s.substring(movingIndex, movingIndex + wordLength);
+
+           if(!copyWordSet.contains(word)){
+               return;
+           }
+
+           copyWordSet.remove(word);
+           movingIndex += wordLength;
+       }
+
+       if(copyWordSet.isEmpty()){
+           indexes.add(actualIndex);
+       }
     }
 }
