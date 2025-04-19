8ec43641efd7cdaeadf1c7bc709047aa6509669d
David Cediel <esdago18@gmail.com>
2025-04-19 12:50:25 +0200

    Code first part of problem 31

--- src/main/java/com/example/demo/nextpermutation/NextPermutation.java
+++ src/main/java/com/example/demo/nextpermutation/NextPermutation.java
@@ -4,5 +4,21 @@ public class NextPermutation {
 
     public void nextPermutation(int[] nums) {
 
+        nums = obtainNextPermutation(nums);
+    }
+
+    public int[] obtainNextPermutation(int[] nums) {
+        for (int i = nums.length - 2; i >= 0; i--) {
+            for (int j = nums.length - 1; j > i; j--) {
+
+                if (nums[j] > nums[i]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    return nums;
+                }
+            }
+        }
+        return new int[0];
     }
 }
---
+++ src/test/java/com/example/demo/nextpermutation/NextPermutationTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.nextpermutation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NextPermutationTest {
+
+    NextPermutation nextPermutation = new NextPermutation();
+
+    @Test
+    void shouldObtainNextPermutation() {
+
+        int[] actualNums = nextPermutation.obtainNextPermutation(new int[]{1, 2, 3});
+
+        assertThat(actualNums)
+                .containsExactly(1, 3, 2);
+    }
+
+    @Test
+    void shouldObtainNextPermutation2() {
+
+        int[] actualNums = nextPermutation.obtainNextPermutation(new int[]{1, 1, 5});
+
+        assertThat(actualNums)
+                .containsExactly(1, 5, 1);
+    }
+
+
+    @Test
+    void shouldObtainNextPermutation3() {
+
+        int[] actualNums = nextPermutation.obtainNextPermutation(new int[]{3, 2, 1});
+
+        assertThat(actualNums)
+                .containsExactly(1, 2, 3);
+    }
+}
