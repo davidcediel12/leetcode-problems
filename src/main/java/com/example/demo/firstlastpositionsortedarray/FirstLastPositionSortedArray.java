4aa94b54e9f826f0f5c059bf69417b828c877fdd
David Cediel <esdago18@gmail.com>
2025-04-26 13:51:57 +0200

    Add skeleton of problem 34

---
+++ src/main/java/com/example/demo/firstlastpositionsortedarray/FirstLastPositionSortedArray.java
@@ -0,0 +1,7 @@
+package com.example.demo.firstlastpositionsortedarray;
+
+public class FirstLastPositionSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[0];
+    }
+}
