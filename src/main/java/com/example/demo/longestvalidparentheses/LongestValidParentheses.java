414b3b4ba40442db1b4011388afd42b7f1355383
David Cediel <esdago18@gmail.com>
2025-04-26 10:35:55 +0200

    Code first part of problem 32

--- src/main/java/com/example/demo/longestvalidparentheses/LongestValidParentheses.java
+++ src/main/java/com/example/demo/longestvalidparentheses/LongestValidParentheses.java
@@ -1,8 +1,31 @@
 package com.example.demo.longestvalidparentheses;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LongestValidParentheses {
 
     public int longestValidParentheses(String s) {
-        return 0;
+
+        Deque<Character> openParentheses = new ArrayDeque<>();
+        int maxLongestValidParentheses = 0;
+        int longestValidParentheses = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                openParentheses.push(c);
+            } else if (openParentheses.isEmpty()) {
+                if (longestValidParentheses > maxLongestValidParentheses) {
+                    maxLongestValidParentheses = longestValidParentheses;
+                }
+                longestValidParentheses = 0;
+            } else {
+                openParentheses.pop();
+                longestValidParentheses += 2;
+            }
+        }
+
+        return Math.max(maxLongestValidParentheses, longestValidParentheses);
     }
 }
---
+++ src/test/java/com/example/demo/longestvalidparentheses/LongestValidParenthesesTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.longestvalidparentheses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestValidParenthesesTest {
+
+
+    LongestValidParentheses validator = new LongestValidParentheses();
+
+
+    @Test
+    void shouldReturnMaxValidParenthesesCorrectly(){
+        assertEquals(2, validator.longestValidParentheses("(()"));
+    }
+
+    @Test
+    void shouldReturnMaxValidParenthesesCorrectly2(){
+        assertEquals(4, validator.longestValidParentheses(")()())"));
+    }
+
+    @Test
+    void shouldReturnMaxValidParenthesesCorrectly3(){
+        assertEquals(0, validator.longestValidParentheses(""));
+    }
+
+    @Test
+    void shouldReturnMaxValidParenthesesCorrectly4(){
+        assertEquals(2, validator.longestValidParentheses("()(()"));
+    }
+
+}
