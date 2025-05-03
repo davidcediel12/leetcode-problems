fb452fcece807a42a50bcef2531f873870e15341
David Cediel <esdago18@gmail.com>
2025-05-03 12:03:24 +0200

    Add first solution of problem 36

--- src/main/java/com/example/demo/validsudoku/ValidSudoku.java
+++ src/main/java/com/example/demo/validsudoku/ValidSudoku.java
@@ -1,8 +1,54 @@
 package com.example.demo.validsudoku;
 
+import java.util.*;
+
 public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
-        return false;
+        Map<String, Set<Character>> subBoxes = HashMap.newHashMap(9);
+        List<Set<Character>> rows = new ArrayList<>(9);
+        List<Set<Character>> cols = new ArrayList<>(9);
+
+        for (int row = 0; row < board.length; row++) {
+            if (rows.size() <= row + 1) {
+                rows.add(new HashSet<>());
+            }
+            for (int col = 0; col < board[row].length; col++) {
+
+                if (cols.size() <= col + 1) {
+                    cols.add(new HashSet<>());
+                }
+
+                char character = board[row][col];
+
+                if (character == '.') {
+                    continue;
+                }
+
+                if (rows.get(row).contains(character)) {
+                    return false;
+                }
+                rows.get(row).add(character);
+
+
+                if (cols.get(col).contains(character)) {
+                    return false;
+                }
+                cols.get(col).add(character);
+
+                String key = obtainSubBox(row, col);
+
+                subBoxes.putIfAbsent(key, new HashSet<>());
+                if (subBoxes.get(key).contains(character)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    private String obtainSubBox(int row, int col) {
+        return (row / 3) + ":" + (col / 3);
     }
 }
---
+++ src/test/java/com/example/demo/validsudoku/ValidSudokuTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.validsudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValidSudokuTest {
+
+    ValidSudoku validator = new ValidSudoku();
+
+    @Test
+    void shouldValidateCorrectly() {
+
+        char[][] sudoku = new char[][]{
+                new char[]{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        assertTrue(validator.isValidSudoku(sudoku));
+    }
+
+
+    @Test
+    void shouldValidateCorrectly2() {
+
+        char[][] sudoku = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        assertFalse(validator.isValidSudoku(sudoku));
+    }
+
+}
