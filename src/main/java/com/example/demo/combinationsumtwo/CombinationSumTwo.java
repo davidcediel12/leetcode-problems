57f9507b75a495a1d700d38adf01f811348e2ad3
David Cediel <esdago18@gmail.com>
2025-08-20 15:13:48 -0500

    Add skeleton of problem 40

---
+++ src/main/java/com/example/demo/combinationsumtwo/CombinationSumTwo.java
@@ -0,0 +1,12 @@
+package com.example.demo.combinationsumtwo;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CombinationSumTwo {
+
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        return Collections.emptyList();
+    }
+}
