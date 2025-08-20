5263dceb4c49415619bc3d3e30424c4faf5bea2f
David Cediel <esdago18@gmail.com>
2025-08-20 10:38:49 -0500

    Add skeleton of problem 39

---
+++ src/main/java/com/example/demo/combinationsum/CombinationSum.java
@@ -0,0 +1,12 @@
+package com.example.demo.combinationsum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return new ArrayList<>();
+    }
+}
