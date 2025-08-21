7d039fa211410258d2754e3e90a914cd32223d95
David Cediel <esdago18@gmail.com>
2025-08-20 21:46:15 -0500

    Add first solution of problem 40, is the same backtracking but not allowing repeating the numbers by adding one to the backtrack iterator

--- src/main/java/com/example/demo/combinationsumtwo/CombinationSumTwo.java
+++ src/main/java/com/example/demo/combinationsumtwo/CombinationSumTwo.java
@@ -1,12 +1,36 @@
 package com.example.demo.combinationsumtwo;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class CombinationSumTwo {
 
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return Collections.emptyList();
+
+        Arrays.sort(candidates);
+
+        Set<List<Integer>> options = new HashSet<>();
+
+        combinationSum2(candidates, target, 0, new ArrayList<>(), options);
+        return options.stream().toList();
+    }
+
+    private void combinationSum2(int[] candidates, int target, int start,
+                                 List<Integer> currentOption, Set<List<Integer>> options) {
+
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            options.add(new ArrayList<>(currentOption));
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            currentOption.add(candidates[i]);
+            combinationSum2(candidates, target - candidates[i], i + 1, currentOption, options);
+            currentOption.removeLast();
+        }
+
     }
 }
---
+++ src/test/java/com/example/demo/combinationsumtwo/CombinationSumTwoTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.combinationsumtwo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CombinationSumTwoTest {
+
+
+    CombinationSumTwo combinationSumTwo = new CombinationSumTwo();
+
+
+    @Test
+    void shouldReturnCorrectOptions() {
+
+        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
+
+        List<List<Integer>> expectedOptions = List.of(
+                List.of(1, 1, 6),
+                List.of(1, 2, 5),
+                List.of(1, 7),
+                List.of(2, 6)
+        );
+
+        var actualOptions = combinationSumTwo.combinationSum2(candidates, 8);
+
+        assertThat(actualOptions)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(expectedOptions);
+    }
+
+
+    @Test
+    void shouldReturnCorrectOptions2() {
+
+        int[] candidates = new int[]{2,5,2,1,2};
+
+        List<List<Integer>> expectedOptions = List.of(
+                List.of(1, 2, 2),
+                List.of(5)
+        );
+
+        var actualOptions = combinationSumTwo.combinationSum2(candidates, 5);
+
+        assertThat(actualOptions)
+                .usingRecursiveComparison()
+                .ignoringCollectionOrder()
+                .isEqualTo(expectedOptions);
+    }
+
+}
