9dcbd8ff75cadbbdaad2150193b86532718aa313
David Cediel <esdago18@gmail.com>
2025-08-27 20:39:43 -0500

    Add first solution of problem 42 by adding water from both sides

--- src/main/java/com/example/demo/trappingrainwater/TrappingRainWater.java
+++ src/main/java/com/example/demo/trappingrainwater/TrappingRainWater.java
@@ -1,8 +1,71 @@
 package com.example.demo.trappingrainwater;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TrappingRainWater {
 
     public int trap(int[] height) {
-        return 0;
+
+        int leftWallIndex = -1;
+        int leftWallHeight = -1;
+
+        int water = 0;
+        List<Integer> heights = Arrays.stream(height).boxed().toList();
+
+        for (int i = 0; i < height.length; i++) {
+            int actualHeight = height[i];
+
+            if (actualHeight > leftWallHeight) {
+
+                if (leftWallHeight > 0) {
+                    water += calculateWater(heights, i, leftWallIndex);
+                }
+                leftWallHeight = actualHeight;
+                leftWallIndex = i;
+
+            }
+        }
+
+        int rightWallIndex = -1;
+        int rightWallHeight = -1;
+
+        for (int i = height.length - 1; i >= 0; i--) {
+            int actualHeight = height[i];
+
+            if (actualHeight > rightWallHeight) {
+
+                if (rightWallHeight > 0) {
+                    water += calculateWaterReverseOrder(heights, i, rightWallIndex);
+                }
+                rightWallHeight = actualHeight;
+                rightWallIndex = i;
+
+            }
+        }
+
+
+        return water;
+    }
+
+    private int calculateWater(List<Integer> heights, int actualIndex, int leftWallIndex) {
+
+
+        Integer obstacles = heights.subList(leftWallIndex + 1, actualIndex).stream()
+                .reduce(Integer::sum)
+                .orElse(0);
+
+        return heights.get(leftWallIndex) * (actualIndex - leftWallIndex - 1) - obstacles;
+    }
+
+
+    private int calculateWaterReverseOrder(List<Integer> heights, int actualIndex, int rightWallIndex) {
+
+
+        Integer obstacles = heights.subList(actualIndex + 1, rightWallIndex).stream()
+                .reduce(Integer::sum)
+                .orElse(0);
+
+        return heights.get(rightWallIndex) * (rightWallIndex - actualIndex - 1) - obstacles;
     }
 }
---
+++ src/test/java/com/example/demo/trappingrainwater/TrappingRainWaterTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.trappingrainwater;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrappingRainWaterTest {
+
+    TrappingRainWater trappingRainWater = new TrappingRainWater();
+
+
+    @Test
+    void shouldCalculateTrappingWaterCorrectly(){
+
+        int actualWater = trappingRainWater.trap(new int[]{4, 2, 0, 3, 2, 5});
+
+        assertEquals(9, actualWater);
+
+    }
+
+
+    @Test
+    void shouldCalculateTrappingWaterCorrectly2(){
+
+        int actualWater = trappingRainWater.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
+
+        assertEquals(6, actualWater);
+
+    }
+}
