ade974e45d518eeb8297e1bf6558a5e18c8fb872
David Cediel <esdago18@gmail.com>
2025-09-08 10:42:04 -0500

    Add first solution of problem 43

--- src/main/java/com/example/demo/multiplystrings/MultiplyStrings.java
+++ src/main/java/com/example/demo/multiplystrings/MultiplyStrings.java
@@ -1,8 +1,67 @@
 package com.example.demo.multiplystrings;
 
+import java.util.Map;
+
 public class MultiplyStrings {
 
+
+    Map<Character, Long> numbers = Map.of(
+            '0', 0L,
+            '1', 1L,
+            '2', 2L,
+            '3', 3L,
+            '4', 4L,
+            '5', 5L,
+            '6', 6L,
+            '7', 7L,
+            '8', 8L,
+            '9', 9L
+    );
+
+    Map<Long, Character> reversedNumbers = Map.of(
+            0L, '0',
+            1L, '1',
+            2L, '2',
+            3L, '3',
+            4L, '4',
+            5L, '5',
+            6L, '6',
+            7L, '7',
+            8L, '8',
+            9L, '9'
+    );
+
+
     public String multiply(String num1, String num2) {
-        return "";
+
+
+        Long result = transform(num1) * transform(num2);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (result / 10 > 0){
+            sb.insert(0, result % 10);
+            result = result / 10;
+        }
+        sb.insert(0, result);
+        return sb.toString();
+
+    }
+
+
+    private Long transform(String num){
+
+        int count = 0;
+        Long transformedNum = 0L;
+
+        for (int i = num.length() - 1; i >= 0; i--) {
+
+            Character c = num.charAt(i);
+            Long digit = numbers.get(c);
+            transformedNum += digit * (long) Math.pow(10, count);
+            count++;
+        }
+
+        return transformedNum;
     }
 }
---
+++ src/test/java/com/example/demo/multiplystrings/MultiplyStringsTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.multiplystrings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class MultiplyStringsTest {
+
+
+    MultiplyStrings multiplyStrings = new MultiplyStrings();
+
+    @Test
+    void shouldMultiplyCorrectly(){
+
+        assertEquals("6", multiplyStrings.multiply("3", "2"));
+    }
+
+    @Test
+    void shouldMultiplyCorrectly2(){
+
+        assertEquals("1316", multiplyStrings.multiply("47", "28"));
+    }
+
+
+    @Test
+    void shouldMultiplyCorrectly3(){
+
+        assertEquals("56088", multiplyStrings.multiply("123", "456"));
+    }
+}
