223750826e77fb933de89655f03d03d23e14b884
David Cediel <esdago18@gmail.com>
2025-09-22 11:23:55 -0500

    Remove all prunes since now we just have one recursion

--- src/main/java/com/example/demo/jumpgametwo/JumpGameTwo.java
+++ src/main/java/com/example/demo/jumpgametwo/JumpGameTwo.java
@@ -1,13 +1,7 @@
 package com.example.demo.jumpgametwo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class JumpGameTwo {
 
-    int minJump = Integer.MAX_VALUE;
-
-    Map<Integer, Integer> jumpsByPosition = new HashMap<>();
 
     public int jump(int[] nums) {
         return jump(nums, 0, 0);
@@ -16,24 +10,16 @@ public class JumpGameTwo {
 
     private int jump(int[] nums, int actualIndex, int jumps) {
 
-        if (jumpsByPosition.containsKey(actualIndex) && jumpsByPosition.get(actualIndex) <= jumps) {
-            return jumpsByPosition.get(actualIndex);
-        }
-
-        if (jumps >= this.minJump) {
-            jumpsByPosition.put(actualIndex, jumps);
-            return jumps;
-        }
 
         int maxForwardJump = nums[actualIndex];
 
         if (actualIndex == nums.length - 1) {
-            jumpsByPosition.put(actualIndex, jumps);
+
             return jumps;
         }
 
         if (actualIndex + maxForwardJump >= nums.length - 1) {
-            jumpsByPosition.put(actualIndex, jumps + 1);
+
             return jumps + 1;
         }
 
@@ -51,13 +37,6 @@ public class JumpGameTwo {
             i++;
         }
 
-        int requiredJumps = jump(nums, positionToJump, jumps + 1);
-
-        if (requiredJumps < this.minJump) {
-            this.minJump = requiredJumps;
-        }
-
-        jumpsByPosition.put(actualIndex, this.minJump);
-        return this.minJump;
+        return jump(nums, positionToJump, jumps + 1);
     }
 }
