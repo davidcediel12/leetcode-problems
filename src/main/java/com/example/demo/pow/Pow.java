b4b45334b8705e7d70797ae7014734dc4e4da159
David Cediel <esdago18@gmail.com>
2025-10-03 15:00:47 -0500

    Wrap the inefficient solution in an inner class

--- src/main/java/com/example/demo/pow/Pow.java
+++ src/main/java/com/example/demo/pow/Pow.java
@@ -2,38 +2,46 @@ package com.example.demo.pow;
 
 public class Pow {
 
-    public double myPow(double x, int n) {
+    static class InefficientSolution {
+        public double myPow(double x, int n) {
 
-        double result = 1;
+            double result = 1;
 
-        if(n == 0 || x == 1){
-            return 1D;
-        }
-
-        if(x == 0D || (n == Integer.MIN_VALUE && x != -1)){
-            return 0D;
-        }
-
-        if(x == -1D){
-            if(n % 2 == 0){
+            if (n == 0 || x == 1) {
                 return 1D;
             }
-            return -1D;
-        }
 
-        boolean isNegative = n < 0;
-        if(isNegative){
-            n = n * -1;
-        }
+            if (x == 0D || (n == Integer.MIN_VALUE && x != -1)) {
+                return 0D;
+            }
 
-        while(n > 0){
-            result = result * x;
-            n--;
-        }
+            if (x == -1D) {
+                if (n % 2 == 0) {
+                    return 1D;
+                }
+                return -1D;
+            }
 
-        if(isNegative){
-            return 1/result;
+            boolean isNegative = n < 0;
+            if (isNegative) {
+                n = n * -1;
+            }
+
+            while (n > 0) {
+                result = result * x;
+                n--;
+            }
+
+            if (isNegative) {
+                return 1 / result;
+            }
+            return result;
         }
-        return result;
     }
+
+
+    public double myPow(double x, int n) {
+        return 0D;
+    }
+
 }
