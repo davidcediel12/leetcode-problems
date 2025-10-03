9d7076d9e060463368ce79af6dffa1a4226cfba0
David Cediel <esdago18@gmail.com>
2025-10-03 12:37:51 -0500

    Use a map to store anagrams to avoid iterate over the whole list to check if it exists
    The key is the sorted word

--- src/main/java/com/example/demo/groupanagrams/GroupAnagrams.java
+++ src/main/java/com/example/demo/groupanagrams/GroupAnagrams.java
@@ -1,52 +1,25 @@
 package com.example.demo.groupanagrams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupAnagrams {
 
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        List<List<String>> anagrams = new ArrayList<>();
+        Map<String, List<String>> anagrams = new HashMap<>();
 
 
-        for (int i = 0; i < strs.length; i++) {
+        for (String word : strs) {
 
-            String word = strs[i];
+            String key = Arrays.stream(word.split("")).sorted().collect(Collectors.joining());
 
-            boolean added = false;
-            int j = 0;
-
-            while (!added && j < anagrams.size()) {
-
-                String existingWord = anagrams.get(j).stream().findFirst().get();
-
-                if (existingWord.length() != word.length()) {
-                    j++;
-                    continue;
-                }
-
-                List<String> charsExisting = Arrays.stream(existingWord.split(""))
-                        .sorted()
-                        .toList();
-
-                List<String> charsWord = Arrays.stream(word.split(""))
-                        .sorted()
-                        .toList();
-
-                if (Objects.equals(charsExisting, charsWord)) {
-                    anagrams.get(j).add(word);
-                    added = true;
-                }
-                j++;
-            }
-
-            if (!added) {
-                anagrams.add(new ArrayList<>(List.of(word)));
+            if (anagrams.containsKey(key)) {
+                anagrams.get(key).add(word);
+            } else {
+                anagrams.put(key, new ArrayList<>(List.of(word)));
             }
         }
-        return anagrams;
+        return anagrams.values().stream().toList();
     }
 }
